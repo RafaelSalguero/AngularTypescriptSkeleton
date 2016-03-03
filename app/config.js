14c88040677aeeda22edc0b0c5debeb4aa1a7a78
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-03-03 10:58:38 -0700

    updated autoreq

--- app/config.js
+++ app/config.js
@@ -3,11 +3,14 @@ console.log("config run");
 require.config({
     paths: {
         'angular': "/node_modules/angular/angular",
-        'angular-ui-router': "/node_modules/angular-ui-router/release/angular-ui-router"
+        'angular-ui-router': "/node_modules/angular-ui-router/release/angular-ui-router",
+        'jQuery': "https://code.jquery.com/jquery-2.2.1.min.js",
+        'bootstrap': "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
     },
     shim: {
         'angular': { exports: 'angular' },
-        'angular-ui-router': { exports: 'angular-ui-router', deps: ['angular'] }
+        'angular-ui-router': { exports: 'angular-ui-router', deps: ['angular'] },
+        'bootstrap': { deps: ['jQuery'] }
     },
     deps: ["main/main"]
 });
