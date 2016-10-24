3969e629db82216ac03da58448f5968ab00cfbeb
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-10-24 15:36:14 -0700

    removed packages

--- tools/build.js
+++ tools/build.js
@@ -5,9 +5,19 @@
 var exec = (x) => x.forEach(y=> { 
     var r = require('child_process').execSync(y, {encoding: "utf8" } ); if (r != null && r != "") console.log(r);
 });
+
+var versionRegex =`application.version\\s*=\\s'\\S*'\\s*;\\s*//__version`;
+var now = new Date();
+var versionReplace =`application.version = '${now.toISOString()}'; //__version`;
+
+var versionCmd = `node node_modules/replace/bin/replace.js "${versionRegex}" "${versionReplace}" Index.html`;
+
 exec([
-    //genera cache busting version:
-    'node node_modules/dateversion/dateversion.js "app/version.js" "application.version"',
+    //Copia el index_TEMPLATE.html al index, esto para evitar tener al index adentro del control de código,
+    //ya que con cada compilación esta cambiando
+    'COPY /Y "index_TEMPLATE.html" "index.html',
+    //Reemplaza la versión del Index.html:
+    versionCmd,
     
     //generate requires for polyfills:
     'node node_modules/autoreq/autoreq.js *.js . app\\polyfills\\requireme.ts app\\polyfills',
